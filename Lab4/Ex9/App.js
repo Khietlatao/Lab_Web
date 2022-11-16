65be18c3c346131cc98513e743247cc2aedad626
Trương Minh Khiết <19520628@gm.uit.edu.vn>
2022-11-16 22:22:18 +0700

    commit

---
+++ Lab4/Ex9/App.js
@@ -0,0 +1,65 @@
+import "typeface-roboto";
+import React,{useState} from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import  IconButton  from "@mui/material/IconButton";
+import AndroidIcon from "@mui/material/Androids";
+
+const buttonStyle = { margin:10};
+
+function toggleColor (setter, value ) {
+  setter( value === "default" ? "primary": "default");
+}
+
+export default function App () {
+  const [contained, setContained]= useState("default");
+  const [text, setText ] = useState ("default");
+  const [outlined, setOutlined] = useState("default");
+  const [icon, setIcon] = useState("default");
+
+  return (
+    <Grid container>
+      <Grid
+        item 
+        component ={Button}
+        variant="contained"
+        style = {buttonStyle}
+        color = {contained}
+        onClick ={()=>toggleColor (setContained, contained)}
+        >
+          Contained
+        </Grid>
+      <Grid
+        item
+        component={Button}
+        style = {buttonStyle}
+        color={text }
+        onClick ={()=> toggleColor(setText, text) }
+      >
+        Text
+      </Grid>
+      <Grid
+        item 
+        component ={Button}
+        variant="outlined"
+        style = {buttonStyle}
+        color = {contained}
+        onClick ={()=>toggleColor (setContained, outlined)}
+      >
+        Outlined
+      </Grid>
+      <Grid
+        item 
+        component ={IconButton}
+        variant="outlined"
+        style = {buttonStyle}
+        color = {icon}
+        onClick ={()=>toggleColor (setContained, icon)}
+      >
+          <AndroidIcon  />
+      </Grid>
+    </Grid>
+
+  
+  );
+}
